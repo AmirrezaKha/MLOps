78b1760aca1fc404c6086702fc8847933590bec4
Amirreza Khamehchin Khiabani <91806036+AmirrezaKha@users.noreply.github.com>
2024-08-04 02:13:03 +0200

    Initial commit.

--- R/R/install_packages.R
+++
@@ -1,9 +0,0 @@
-packages <- c("tidyverse", "ggplot2", "rmarkdown")
-
-install_if_missing <- function(p) {
-  if (!requireNamespace(p, quietly = TRUE)) {
-    install.packages(p, repos = "https://cran.rstudio.com/", dependencies = TRUE)
-  }
-}
-
-invisible(lapply(packages, install_if_missing))
